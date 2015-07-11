3e1bb1132c9f3331c53a7d558e176a444f6a8946
Daniel Martí <mvdan@mvdan.cc>
2015-07-11 14:47:10 -0700

    Finish up KML parsing

--- data/kml.go
+++ data/kml.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 )
 
 type kml struct {
@@ -15,15 +16,21 @@ type kml struct {
 
 type placemark struct {
 	Name  string `xml:"name"`
-	Desc  string `xml:"description"`
+	Desc  desc   `xml:"description"`
 	Point point  `xml:"Point"`
 }
 
-type point struct {
-	Coord coord `xml:"coordinates"`
+type desc struct {
+	Lines []struct {
+		Colour struct {
+			Bold string `xml:"b"`
+		} `xml:"font"`
+	} `xml:"p"`
 }
 
-type coord string
+type point struct {
+	Coord string `xml:"coordinates"`
+}
 
 const (
 	communityClubsKML = "communityclubs.kml"
@@ -32,8 +39,47 @@ const (
 func (k *kml) debugPrint() {
 	for _, p := range k.Document.Placemarks {
 		fmt.Println(p.Name)
-		fmt.Println(p.Point)
-		fmt.Println(p.Desc)
+		fmt.Println(p.Point.Coord)
+		for _, l := range p.Desc.Lines {
+			fmt.Println(l.Colour.Bold)
+		}
+	}
+}
+
+type CommunityClub struct {
+	Name  string `json:"name"`
+	Point string `json:"point"`
+	Desc  string `json:"desc"`
+}
+
+var (
+	descMatch = regexp.MustCompile(`Description - (.*)`)
+)
+
+func NewCommunityClubs(k *kml) []CommunityClub {
+	var clubs []CommunityClub
+	for _, p := range k.Document.Placemarks {
+		c := CommunityClub{
+			Name:  p.Name,
+			Point: p.Point.Coord,
+		}
+		for _, l := range p.Desc.Lines {
+			s := l.Colour.Bold
+			if m := descMatch.FindStringSubmatch(s); m != nil {
+				fmt.Println(m)
+				c.Desc = m[1]
+			}
+		}
+		clubs = append(clubs, c)
+	}
+	return clubs
+}
+
+func printCommunityClubs(clubs []CommunityClub) {
+	for _, c := range clubs {
+		fmt.Println(c.Name)
+		fmt.Println(c.Desc)
+		fmt.Println(c.Point)
 	}
 }
 
@@ -44,5 +90,6 @@ func main() {
 	}
 	k := kml{}
 	xml.NewDecoder(f).Decode(&k)
-	k.debugPrint()
+	clubs := NewCommunityClubs(&k)
+	printCommunityClubs(clubs)
 }
