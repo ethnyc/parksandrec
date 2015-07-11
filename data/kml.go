c230a0b1b7048b5d7d733a58b702aed3be09bf6e
Daniel Martí <mvdan@mvdan.cc>
2015-07-11 14:16:23 -0700

    Start parsing the KML file

--- data/kml.go
+++ data/kml.go
@@ -1,4 +1,48 @@
 package main
 
+import (
+	"encoding/xml"
+	"fmt"
+	"log"
+	"os"
+)
+
+type kml struct {
+	Document struct {
+		Placemarks []placemark `xml:"Placemark"`
+	}
+}
+
+type placemark struct {
+	Name  string `xml:"name"`
+	Desc  string `xml:"description"`
+	Point point  `xml:"Point"`
+}
+
+type point struct {
+	Coord coord `xml:"coordinates"`
+}
+
+type coord string
+
+const (
+	communityClubsKML = "communityclubs.kml"
+)
+
+func (k *kml) debugPrint() {
+	for _, p := range k.Document.Placemarks {
+		fmt.Println(p.Name)
+		fmt.Println(p.Point)
+		fmt.Println(p.Desc)
+	}
+}
+
 func main() {
+	f, err := os.Open(communityClubsKML)
+	if err != nil {
+		log.Fatal(err)
+	}
+	k := kml{}
+	xml.NewDecoder(f).Decode(&k)
+	k.debugPrint()
 }
